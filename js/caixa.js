f9b29f5ccc0a2183eab2fcd276a6b61a4c7cec26
leonardoguido3 <leonardoguido3@gmail.com>
2023-04-23 00:24:44 -0300

    ajustes no consumo da API para visualizacao de clientes

--- js/caixa.js
+++ js/caixa.js
@@ -2,11 +2,12 @@ var urlBaseApi = "https://localhost:44394/";
 var tabelaCaixa;
 var cardCliente;
 
-// LISTAR CAIXAS
+// Função principal do index para listar caixas
 $(document).ready(function () {
     ListarCaixas();
 });
 
+// Chamada API para listar caixas
 function ListarCaixas() {
     var rotaApi = "caixa";
 
@@ -22,35 +23,91 @@ function ListarCaixas() {
     });
 }
 
-// CONSTRUCAO DE TABELAS
-function ConstruirTabelaCaixa(linhas) {
+// Chamada API para listar caixa especifica
+function ListarCaixaEspecifica(id) {
+    var rotaApi = "caixa/" + id;
 
-    var htm = "";
-
-    $(linhas).each(function (index, linha) {
-        htm = htm + `<tr id="visualiza" onclick="VisualizarCaixa(${linha.id})"><th>${linha.nome}</th><td>${linha.referencia}
-        </td><td>${linha.rua}</td><td>${linha.bairro}</td><td>${linha.cidade}</td><th>${linha.clientes}</th></tr>`;
-    });
-
-    $("#tabelaCaixa tbody").html(htm);
-    if (tabelaCaixa == undefined) {
-        tabelaCaixa = $('#tabelaCaixa').DataTable();
-    }
-    $(".preloading").hide();
-    var celula = document.getElementsById("visualiza");
-    celula.addEventListener("mouseover", function () {
-        celula.style.backgroundColor = "rgba(0, 120, 0, 0.484)";
-        celula.style.color = "white";
-        celula.style.cursor = "pointer";
-    });
-    celula.addEventListener("mouseout", function () {
-        celula.style.backgroundColor = "white";
-        celula.style.color = "#858796";
-        celula.style.cursor = "default";
+    $.ajax({
+        url: urlBaseApi + rotaApi,
+        method: 'GET',
+        dataType: 'json'
+    }).done(function (conteudoCaixa) {
+        MontarCardsVisualizacao(conteudoCaixa);
     });
 }
 
-// CAPTURANDO VALORES PARA POST
+// Construção da tabela de caixas cadastradas
+function ConstruirTabelaCaixa(caixas) {
+
+    var caixaTabelaHtml = "";
+
+    $(caixas).each(function (index, caixa) {
+        caixaTabelaHtml = caixaTabelaHtml + `<tr id="visualiza" data-id="${caixa.id}"><th>${caixa.nome}</th><td>${caixa.referencia}</td><td>${caixa.rua}</td><td>${caixa.bairro}</td><td>${caixa.cidade}</td><th>${caixa.clientes}</th></tr>`;
+    });
+
+    $("#tabelaCaixa tbody").html(caixaTabelaHtml);
+    if (tabelaCaixa == undefined) {
+        tabelaCaixa = $('#tabelaCaixa').DataTable();
+    }
+
+    // Seleciona todas as células com id "visualiza"
+    var celulas = document.querySelectorAll("#tabelaCaixa tbody tr#visualiza");
+
+    // Itera sobre cada célula e adiciona os eventos de mouseover e mouseout
+    celulas.forEach(function (celula) {
+        celula.addEventListener("mouseover", function () {
+            celula.style.backgroundColor = "rgba(0, 120, 0, 0.484)";
+            celula.style.color = "white";
+            celula.style.cursor = "pointer";
+        });
+        celula.addEventListener("mouseout", function () {
+            celula.style.backgroundColor = "white";
+            celula.style.color = "#858796";
+            celula.style.cursor = "default";
+        });
+    });
+    $(".preloading").hide();
+
+    // Adiciona manipulador de eventos de clique para cada linha da tabela
+    $("#tabelaCaixa tbody").on("click", "tr", function () {
+        // Obtém o ID da caixa da linha clicada
+        var id = $(this).attr("data-id");
+        // enviando id para montar card
+        ListarCaixaEspecifica(id);
+
+    });
+}
+
+// Visualizacao dos clientes cadastrados na caixa
+function MontarCardsVisualizacao(conteudoCaixa) {
+
+    $("#containerCards").empty();
+    $.get("/caixas/visualizacao.html", function (data) {
+
+        // Monta os cards
+        $(conteudoCaixa).each (function (index, cliente) {
+            if (cliente != null) {
+                var cardHtml = `
+                    <div class="col-xl-3 col-md-6 mb-4">
+                        <div class="card border-left-success shadow h-100 py-2">
+                            <div class="card-body" id="cardCaixa">
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col mr-2 content-card">
+                                        <div class="text-xs font-weight-bold text-success text-uppercase mb-1">${cliente.nome}</div>
+                                        <div class="h5 mb-0 font-weight-bold text-gray-800">${cliente.conta}</div>
+                                        <div class="h6 mb-0 font-weight-bold text-gray-500">${cliente.caixa}</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`;
+                $("#containerCards").append(cardHtml);    
+            }
+        });
+    })
+}
+
+// Capturando os valores para POST API
 function ObterValoresFormulario() {
     var nome = $("#inputNome").val();
     var referencia = $("#inputReferencia").val();
@@ -69,6 +126,7 @@ function ObterValoresFormulario() {
     return objeto;
 }
 
+// Enviando o formulário de cadastro para API
 function EnviarFormularioParaApi() {
     $(".preloading").show();
 
@@ -97,58 +155,12 @@ function EnviarFormularioParaApi() {
     });
 }
 
+// Funcao validadora do formulario e chamada para funcao de Enviar
 function SubmeterFormulario() {
     var isValido = $('#formCaixa');
     if (isValido) {
         EnviarFormularioParaApi();
     }
-}
-function RedirecionarListagem() {
-    window.location.href = "/caixas/index.html";
-}
-
-
-function VisualizarCaixa(linha) {
-    window.location.href = "/caixas/visualizacao.html";
-    ListarCaixaId(id);
-}
-
-function ListarCaixaId(id) {
-    var rotaApi = "caixa/";
-
-    $.ajax({
-        url: urlBaseApi + rotaApi + id,
-        method: 'GET',
-        dataType: 'json',
-        // beforeSend: function (xhr){
-        //     xhr.setRequestHeader('Authorization', 'Bearer' + localStorage.getItem('bearer'));
-        // }
-    }).done(function (conteudoApi) {
-        ConstruirCardCaixa(conteudoApi);
-    });
-}
-
-function ConstruirCardCaixa(linhas) {
-    var htm = "";
-
-    $(linhas).each(function (index, linha) {
-        htm = htm + `<div class="col-xl-3 col-md-6 mb-4">
-        <div class="card border-left-success shadow h-100 py-2">
-            <div class="card-body">
-                <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                            ATIVO</div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800">${linha.nome}</div>
-                        <div class="h6 mb-0 font-weight-bold text-gray-500">${linha.referencia}</div>
-                        <div class="h6 mb-0 font-weight-bold text-gray-500">${linha.rua}</div>
-                    </div>
-                </div>
-            </div>
-        </div>`;
-    });
-
-    $("#cardCaixa div").html(htm);
 }
 
 // lendo o documento e inserindo as mascaras
